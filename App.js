6143f495258b2e5a7e0f29a03f46693c5af7006b
HarishTarun17 <132081283+HarishTarun17@users.noreply.github.com>
2024-04-29 19:02:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUpPage from './SignUpPage';
 import LoginPage from './Login';
-import Home from './Home';
+import HomePage from './Home';
+import PlaceOrderPage from './PlaceOrder';
+import ConfirmationPage from './Confirmation';
 
 function App() {
   return (
@@ -9,8 +12,11 @@ function App() {
       <Routes>
         <Route path="/" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/place-order" element={<PlaceOrderPage />} />
+        <Route path="/confirmation" element={<ConfirmationPage />} />
       </Routes>
+      
     </Router>
   );
 }
